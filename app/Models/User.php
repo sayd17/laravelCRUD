795d6989bee05eaa6aaace3ea28c59f45b0b89fc
sayd17 <sayeed1737@gmail.com>
2024-02-22 20:05:19 +0600

    Created Many to Many relationship successfully

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,18 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function profile(){
+        $this->hasOne(Profile::class);
+
+    }
+
+    public function post(){
+        $this->hasMany(Post::class);
+
+    }
+
+    
+
+
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,11 +12,6 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -27,18 +22,9 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
+            'experience_points' => 0
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
